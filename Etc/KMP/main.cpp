98b867197b2f6ecf48e093dd30f0ad65200b5918
cubeindex-home <pureindex@naver.com>
2022-07-16 13:07:01 +0900

    초기 커밋.

---
+++ Etc/KMP/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int KMPfind(const std::string& s, const std::string& pat, int index = 0)
+{
+	// for failure func
+	static std::vector<int> pi(pat.size());
+	for (auto &e : pi) e = -1;
+
+
+}
+
+
+int main()
+{
+	return 0;
+}
